8273c369d32e4bead379384a07f0e4cc82dd0ea6
mattymatt1994 <jedimatt.jordan@gmail.com>
2023-05-31 19:41:46 -0400

    loops

--- app.js
+++ app.js
@@ -3,5 +3,43 @@ console.log("Hello World!\n==========\n");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
+for (let i = 1; i <= 100; i++) {
+  if (i % 2) {
+    console.log(`Congrats! It's odd: ${i}`);
+  } else {
+    console.log("Even numbers = trash");
+  }
+}
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+for (let i = 0; i <= 100; i++) {
+  let input = " ";
+  
+  if (i % 3) {
+    input += "FIZZ";
+  
+  }
+  if (i % 5) {
+    input += "BUZZ";
+  }
+ console.log(i, input);
+};
+
+//Exercise 3
+console.log("EXERCISE 3");
+let i = 1;
+while (i <= 100) {
+  if (i % 2 == 0) {
+    console.log(`Congrats! It's odd: ${i}`);
+  } else {
+    console.log("Even numbers = trash");
+  }
+}
+
+while (i <= 100) {
+  if (i % 3 == 0) {
+    console.log("FIZZ");
+  } if (i % 5 == 0) {
+    console.log("BUZZ");
+  }
+}
